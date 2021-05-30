57bfdc67a30900f354b1bd6337e44843c6800695
Kaznacheev <antkaznacheev@gmail.ru>
2021-05-30 11:34:35 +0300

    Добавленна форма при старте

--- Lessons2.03/ViewController.swift
+++ Lessons2.03/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var inputUserPole: UITextField!
+    @IBOutlet var inputPasswordPole: UITextField!
+    
+    @IBOutlet var bottomUserNameHelp: UIButton!
+    @IBOutlet var bottomPasswordHelp: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let startPole = UIAlertController(title: "👀", message: "Вам нужно заполнить поля:\n User и Password", preferredStyle: .alert)
+        startPole.addAction(UIAlertAction(title:"Хорошо", style: .cancel, handler:  nil))
+        
+        self.present(startPole, animated: true)
+        
+        
+        
+        
     }
 
+    
+    @IBAction func avtivBottomUserHelp(_ sender: UIButton) {
+        let alertUser = UIAlertController(title: "Ooops!", message:"Yuar name is User 😤", preferredStyle: .alert)
+        
+        alertUser.addAction(UIAlertAction(title:"OK", style: .cancel, handler: nil))
+        
+        self.present(alertUser, animated: true)
+    }
+    
+    @IBAction func activeBottomPasswordHelp() {
+        let alertPassword = UIAlertController(title: "Ooops!", message: "Your password is Password  😤", preferredStyle: .alert)
+        alertPassword.addAction(UIAlertAction(title:"OK", style: .cancel, handler: nil))
+        
+        self.present(alertPassword, animated: true)
+    }
+   
 
 }
 
