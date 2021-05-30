aaf650470de18d9b71d254c75237d6e1cc5afc96
Kaznacheev <antkaznacheev@gmail.ru>
2021-05-30 09:34:47 +0300

    Initial Commit

---
+++ Lessons2.03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lessons2.03
+//
+//  Created by Kaznacheev on 30.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
